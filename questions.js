86cc692071198cce3fffebbebb164f3b47564ef2
youssefRaja20 <youssefyahiamad@gmail.com>
2024-12-23 14:02:53 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,72 +1,58 @@
-// creating an array and passing the number, questions, options, and answers
 let questions = [
     {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
-  },
+      numb: 1,
+      question: "Which country won the 2018 FIFA World Cup?",
+      answer: "France",
+      options: [
+        "Brazil",
+        "Germany",
+        "France",
+        "Argentina"
+      ]
+    },
     {
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
-  },
+      numb: 2,
+      question: "Who won the Ballon d'Or in 2020?",
+      answer: "Lionel Messi",
+      options: [
+        "Cristiano Ronaldo",
+        "Lionel Messi",
+        "Robert Lewandowski",
+        "Virgil van Dijk"
+      ]
+    },
     {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
-  },
+      numb: 3,
+      question: "Who won the 2014 FIFA World Cup Golden Boot?",
+      answer: "James Rodríguez",
+      options: [
+        "Lionel Messi",
+        "Thomas Müller",
+        "James Rodríguez",
+        "Neymar"
+      ]
+    },
     {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
+      numb: 4,
+      question: "Who is the only player to win the Ballon d'Or 7 times?",
+      answer: "Lionel Messi",
+      options: [
+        "Cristiano Ronaldo",
+        "Lionel Messi",
+        "Michel Platini",
+        "Marco van Basten"
+      ]
+    },
     {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
-  },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
-];
+      numb: 5,
+      question: "Who won the Ballon d'Or in 2018?",
+      answer: "Luka Modrić",
+      options: [
+        "Cristiano Ronaldo",
+        "Lionel Messi",
+        "Luka Modrić",
+        "Kylian Mbappé"
+      ]
+    },
+  ];
+  
